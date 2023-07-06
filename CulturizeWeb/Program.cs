34ff7e72e379362b03d8fc7dbccf5ee99506e0a7
Leo R <l_rs.000@proton.me>
2023-07-05 23:38:52 -0300

    fix ?

--- CulturizeWeb/Program.cs
+++ CulturizeWeb/Program.cs
@@ -53,7 +53,7 @@ if (app.Environment.IsDevelopment())
 }
 else
 {
-    app.UseExceptionHandler("User/Home/Error");
+    app.UseExceptionHandler("/User/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
